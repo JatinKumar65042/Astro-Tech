b578fa80aa53338c8738e7a5d88e86675d6ec3ab
JatinKumar65042 <jatinsokhal2300@gmail.com>
2023-10-01 22:13:14 +0530

    added photogallery heading

--- app/page.js
+++ app/page.js
@@ -2,6 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>hello</div>
+    <div>
+      <div className='flex items-center justify-center h-[150px] bg-black '>
+        <h1 className='text-white text-[4.5vw] font-serif'><span className='text-pink-400'>Photo</span> Gallery</h1>
+      </div>
+
+      <div className='flex'>
+        <div>
+          
+        </div>
+
+      </div>
+
+    </div>
   )
 }
